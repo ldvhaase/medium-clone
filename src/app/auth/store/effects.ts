94442cb27d5559e0a46ea7b23401fed336ed31bc
ldvhaase <37157778+ldvhaase@users.noreply.github.com>
2023-12-24 13:59:13 -0500

    add accessToken service

--- src/app/auth/store/effects.ts
+++ src/app/auth/store/effects.ts
@@ -2,25 +2,42 @@ import { inject } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AuthService } from "../services/auth.service";
 import { authActions } from "./actions";
-import { catchError, map, switchMap, of } from "rxjs";
+import { catchError, map, switchMap, of, tap } from "rxjs";
 import { CurrentuserInterface } from "../../shared/types/currentUser.interface";
 import { HttpErrorResponse } from "@angular/common/http";
+import { PersistenceService } from "../../shared/services/persistence.service";
+import { Router } from "@angular/router";
 
 export const registerEffect = createEffect((
   actions$ = inject(Actions),
-  authService = inject(AuthService)
+  authService = inject(AuthService),
+  persistenceService = inject(PersistenceService)
 ) => {
   return actions$.pipe(
     ofType(authActions.register),
     switchMap(({ request }) => {
       return authService.register(request).pipe(
         map((currentUser: CurrentuserInterface) => {
+          persistenceService.set('accessToken', currentUser.token);
           return authActions.registerSuccess({ currentUser })
         }),
         catchError((errorResponse: HttpErrorResponse) => {
-          return of(authActions.registerFailure({errors: errorResponse.error.errors}))
+          return of(authActions.registerFailure({ errors: errorResponse.error.errors }))
         })
       )
     })
   )
-},{ functional: true })
+}, { functional: true })
+
+
+export const redirectAfterRegisterEffect = createEffect(
+  (actions$ = inject(Actions), router = inject(Router)) => {
+    return actions$.pipe(
+      ofType(authActions.registerSuccess),
+      tap(() => {
+        router.navigateByUrl('/')
+      })
+    )
+  },
+  { functional: true, dispatch: false }
+)
---
+++ src/app/shared/services/persistence.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersistenceService {
+
+  constructor() { }
+
+  set(key: string, data: unknown): void {
+    try {
+      localStorage.setItem(key, JSON.stringify(data))
+    } catch (error) {
+      console.error('Error saving to localstorage', error)
+    }
+  }
+
+  get(key: string): unknown {
+    try {
+      const localStorageItem = localStorage.getItem(key);
+      return localStorageItem ? JSON.parse(localStorageItem) : null;
+    } catch (error) {
+      console.error('Error getting from localstorage', error);
+      return null;
+    }
+  }
+}
